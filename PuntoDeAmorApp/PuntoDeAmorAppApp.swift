93b5f94d042c9de389815cf1636cb14e0113d9f5
Ernesto Solis <ernesto@puntodeamor.com>
2026-04-04 03:28:04 -0400

    Initial Commit

---
+++ PuntoDeAmorApp/PuntoDeAmorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PuntoDeAmorAppApp.swift
+//  PuntoDeAmorApp
+//
+//  Created by Ernesto Solis on 4/4/26.
+//
+
+import SwiftUI
+
+@main
+struct PuntoDeAmorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
