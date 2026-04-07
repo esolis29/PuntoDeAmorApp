836d2172bf08749671be21c936b56bf343e93d2d
Ernesto Solis <ernesto@puntodeamor.com>
2026-04-06 21:35:38 -0400

    Respaldo de código real: Login y Vistas principales

---
+++ PuntoDeAmorApp/OracionView.swift
@@ -0,0 +1,18 @@
+//
+//  OracionView.swift
+//  PuntoDeAmorApp
+//
+//  Created by Ernesto Solis on 4/4/26.
+//
+
+import SwiftUI
+
+struct OracionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OracionView()
+}
